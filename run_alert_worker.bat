744f3c7539645a4ff49c894f72bde8fb83c4aa7b
sodia48 <issadiawara800@gmail.com>
2026-06-24 11:53:37 -0400

    Delete run_alert_worker.bat

--- run_alert_worker.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d %~dp0
-if not exist .venv\Scripts\python.exe (
-  echo Lance d'abord run_dashboard.bat pour creer l'environnement.
-  pause
-  exit /b 1
-)
-.venv\Scripts\python.exe alert_worker.py --interval 60
-pause
